19b66ba0d696076e843d09e2404356b466f4fd59
Sayed2210 <eh600039@gmail.com>
2025-09-17 11:44:11 +0300

    layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/AboutView.vue'
+// import HomeView from '../views/AboutView.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'dashboard',
+      component: Dashboard,
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: () => import('../views/AboutView.vue'),
+        },
+      ]
     },
 
   ],
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import DashboardLayout from '@/shared/layouts/DashboardLayout.vue'
+</script>
+
+<template>
+  <dashboard-layout>
+    <RouterView/>
+  </dashboard-layout>
+</template>
+
+<style scoped></style>
