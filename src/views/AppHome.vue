f14769985db626ee2f81cbf2b1ff4cff00675a84
MohabMohamed <mohabmohamedd772@gmail.com>
2025-09-18 11:44:52 +0300

    Data table

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,27 +1,19 @@
 <script setup lang="ts">
-
-
-import CustomSelectInput from '@/shared/FormInputs/CustomSelectInput.vue';
+import DataTable from '@/components/Tables/DataTable.vue';
+import CustomSelectInput from '@/shared/FormInputs/CustomSelectInput.vue'
 import { ref } from 'vue'
 
 const title = ref('')
 </script>
 
 <template>
-
-
+  <div></div>
+  <DataTable />
   <!-- <FileUpload />-->
-
-  <CustomSelectInput
-  :label="'Title'"
-  :static-options="[{ id: 1, title: 'test' }, { id: 2, title: 'test2' }, { id: 3, title: 'test3' }]"
-  :type="2"
-  />
   <!-- <div class="input-wrapper">
     <label for="">Title</label>
     <input type="text" id="" v-model="title" placeholder="Enter Title" />
   </div> -->
-
 </template>
 
 <style scoped></style>
