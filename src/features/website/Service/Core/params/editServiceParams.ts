8eb0a5657dccf73069442682660b24e23ec79a9f
MohamedTarek1993 <www.mtarek789@gmail.com>
2025-10-05 17:13:23 +0300

    update service

--- src/features/website/Service/Core/params/editServiceParams.ts
+++ src/features/website/Service/Core/params/editServiceParams.ts
@@ -4,21 +4,21 @@ import TranslationsParams from '@/base/core/params/translations_params.ts'
 export default class EditServiceParams implements Params {
   id: number
   translation: TranslationsParams
-  alt: string
-  image: string
+  // alt: string
+  // image: string
   includes: TranslationsParams[]
 
   constructor(
     id: number,
     translation: TranslationsParams,
-    alt: string,
-    image: string,
+    // alt: string,
+    // image: string,
     includes: TranslationsParams[],
   ) {
     this.id = id
     this.translation = translation
-    this.alt = alt
-    this.image = image
+    // this.alt = alt/
+    // this.image = image
     this.includes = includes
   }
 
@@ -26,8 +26,8 @@ export default class EditServiceParams implements Params {
     const data: Record<string, unknown> = {}
     data['service_id'] = this.id
     data['translations'] = this.translation.toMap()
-    data['alt'] = this.alt
-    if (this.image == '' || this.image.startsWith('data:image')) data['image'] = this.image
+    // data['alt'] = this.alt
+    // if (this.image == '' || this.image.startsWith('data:image')) data['image'] = this.image
     data['includes'] = this.includes
 
     return data
