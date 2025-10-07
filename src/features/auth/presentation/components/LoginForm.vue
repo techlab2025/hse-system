4aaaf729ab912a4d5a13e31a545d16e10322df53
MohamedTarek1993 <www.mtarek789@gmail.com>
2025-10-07 17:25:32 +0300

    update

--- src/features/auth/presentation/components/LoginForm.vue
+++ src/features/auth/presentation/components/LoginForm.vue
@@ -4,6 +4,10 @@ import LoginController from '@/features/auth/presentation/controllers/login_cont
 import LoginParams from '@/features/auth/Core/Params/login_params'
 import { useRouter } from 'vue-router'
 import { OrganizationTypeEnum } from '../../Core/Enum/organization_type'
+import Email from '@/shared/icons/email.vue'
+import Lock from '@/shared/icons/lock.vue'
+import CloseEye from '@/shared/icons/closeEye.vue'
+import Loca from '@/shared/icons/loca.vue'
 
 const router = useRouter()
 const email = ref('')
@@ -26,8 +30,63 @@ const login = () => {
 
 <template>
   <section class="login">
+    <div class="images">
+      <img src="@/assets/images/login.png" alt="" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="111"
+        height="120"
+        viewBox="0 0 111 120"
+        fill="none"
+      >
+        <g opacity="0.1">
+          <path
+            d="M6.62818 119.845C4.01659 119.845 1.64793 118.265 0.651881 115.848C-0.356318 113.431 0.202443 110.637 2.04878 108.791L37.275 73.5639C39.8137 71.0373 43.9072 71.0373 46.4338 73.5639C48.9725 76.0905 48.9725 80.1962 46.4338 82.7229L11.2076 117.95C10.005 119.164 8.35305 119.845 6.62818 119.845Z"
+            fill="#CAD8FF"
+          />
+          <path
+            d="M110.336 0.354388L18.2259 92.4541C-1.01491 73.213 -1.01491 42.007 18.2259 22.7537C45.6051 -4.62595 110.336 0.354388 110.336 0.354388Z"
+            fill="white"
+          />
+          <path
+            d="M87.9253 92.4523C68.6845 111.706 37.479 111.706 18.2261 92.4523L110.336 0.352539C110.336 0.352539 115.317 65.0726 87.9253 92.4523Z"
+            fill="#CAD8FF"
+          />
+        </g>
+      </svg>
+    </div>
+
     <form @submit.prevent="login" class="login-form">
-      <h1 class="title">HSE Dashboard</h1>
+      <div class="title">
+        <div class="logo">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="28"
+            viewBox="0 0 26 28"
+            fill="none"
+          >
+            <path
+              d="M2.08446 27.6292C1.49046 27.6292 0.951721 27.2701 0.725173 26.7203C0.495862 26.1705 0.62295 25.535 1.04289 25.1151L9.05494 17.1029C9.63236 16.5282 10.5634 16.5282 11.1381 17.1029C11.7155 17.6775 11.7155 18.6114 11.1381 19.186L3.12602 27.1982C2.85251 27.4745 2.47677 27.6292 2.08446 27.6292Z"
+              fill="#1D4ED8"
+            />
+            <path
+              d="M25.6725 0.451698L4.72236 21.3994C0.346126 17.0231 0.346126 9.92541 4.72236 5.54634C10.9497 -0.681059 25.6725 0.451698 25.6725 0.451698Z"
+              fill="#DBE6FF"
+            />
+            <path
+              d="M20.5752 21.3989C16.199 25.778 9.10141 25.778 4.72241 21.3989L25.6725 0.451172C25.6725 0.451172 26.8053 15.1715 20.5752 21.3989Z"
+              fill="#1D4ED8"
+            />
+          </svg>
+          <h2>HSE.Cloud.Ai</h2>
+        </div>
+
+        <div class="text">
+          <p>{{ $t('Log in now') }}</p>
+          <span>{{ $t('Please enter your email and password to log in') }}</span>
+        </div>
+      </div>
 
       <div class="tabs">
         <div
@@ -46,13 +105,28 @@ const login = () => {
         </div>
       </div>
 
-      <div class="input-wrapper">
-        <label for="email" class="input-label">{{ $t('email') }}</label>
-        <input class="input" type="email" id="email" v-model="email" />
-      </div>
-      <div class="input-wrapper">
-        <label for="password" class="input-label">{{ $t('password') }}</label>
-        <input type="password" id="password" class="input" v-model="password" />
+      <div class="inputs">
+        <div class="input-wrapper">
+          <Email class="icon" />
+          <input
+            class="input"
+            :placeholder="$t('enter Your Mail')"
+            type="email"
+            id="email"
+            v-model="email"
+          />
+        </div>
+        <div class="input-wrapper">
+          <Loca class="icon" />
+          <input
+            type="password"
+            id="password"
+            :placeholder="$t('Password')"
+            class="input"
+            v-model="password"
+          />
+          <CloseEye class="icon-eye" />
+        </div>
       </div>
       <button type="submit" class="btn btn-primary">{{ $t('login') }}</button>
     </form>
