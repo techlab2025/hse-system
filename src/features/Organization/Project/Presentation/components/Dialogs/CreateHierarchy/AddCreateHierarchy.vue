9b1e14dccf55eed9ce34350ecd49c2b29b378310
MohamedTarek1993 <www.mtarek789@gmail.com>
2025-10-28 16:23:36 +0300

    add  functional Hierarchy and add your employees , create team Dialogs

---
+++ src/features/Organization/Project/Presentation/components/Dialogs/CreateHierarchy/AddCreateHierarchy.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Dialog from 'primevue/dialog'
+import CreateHierarchy from '@/assets/images/add_Hierarchy.png'
+import { ref } from 'vue'
+import HeaderSection from '../../Details/DetailsHeader/HeaderSection.vue'
+import CreateHierarchyForm from './CreateHierarchyForm.vue'
+const visible = ref(false)
+</script>
+
+<template>
+  <button @click="visible = true">{{ $t('add_Hierarchy') }}</button>
+  <Dialog v-model:visible="visible" modal :dismissable-mask="true" :style="{ width: '50rem' }">
+    <template #header>
+      <div class="add-equipment-header">
+        <HeaderSection
+          :img="CreateHierarchy"
+          title="functional Hierarchy"
+          subtitle="add & delete in your hierarchy for this location"
+        />
+      </div>
+    </template>
+    <div class="equipment-dialog-data">
+      <hr class="add-equipment-hr" />
+      <CreateHierarchyForm />
+    </div>
+  </Dialog>
+</template>
---
+++ src/features/Organization/Project/Presentation/components/Dialogs/CreateTeamDialog/CreateTeamForm.vue
@@ -0,0 +1,85 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import BlueBack from '@/assets/images/BgGroup.png'
+import CustomSelectInput from '@/shared/FormInputs/CustomSelectInput.vue'
+import TitleInterface from '@/base/Data/Models/title_interface'
+import yelloecircle from '@/assets/images/yelloecircle.png'
+const SelectedLocation = ref(1)
+const Locations = ref([
+  {
+    id: 1,
+    title: 'Location 1',
+  },
+  {
+    id: 2,
+    title: 'Location 2',
+  },
+  {
+    id: 3,
+    title: 'Location 3',
+  },
+])
+const Equipment = ref<TitleInterface>()
+const Equipments = ref<TitleInterface[]>([
+  new TitleInterface({ id: 1, title: 'Equipment 1', subtitle: '' }),
+  new TitleInterface({ id: 2, title: 'Equipment 2', subtitle: '' }),
+  new TitleInterface({ id: 3, title: 'Equipment 3', subtitle: '' }),
+])
+const setEquipments = (data: TitleInterface) => {
+  Equipment.value = data
+}
+
+const Device = ref<TitleInterface>()
+const Devices = ref<TitleInterface[]>([
+  new TitleInterface({ id: 1, title: 'Device 1', subtitle: '' }),
+  new TitleInterface({ id: 2, title: 'Device 2', subtitle: '' }),
+  new TitleInterface({ id: 3, title: 'Device 3', subtitle: '' }),
+])
+const setDevices = (data: TitleInterface) => {
+  Device.value = data
+}
+const Tool = ref<TitleInterface>()
+const Tools = ref<TitleInterface[]>([
+  new TitleInterface({ id: 1, title: 'Tool 1', subtitle: '' }),
+  new TitleInterface({ id: 2, title: 'Tool 2', subtitle: '' }),
+  new TitleInterface({ id: 3, title: 'Tool 3', subtitle: '' }),
+])
+const setTool = (data: TitleInterface) => {
+  Tool.value = data
+}
+</script>
+
+<template>
+  <div class="equipment-form">
+    <form>
+      <div class="input-container">
+        <div class="input-wrapper">
+          <CustomSelectInput
+            :modelValue="Equipment"
+            :static-options="Equipments"
+            class="input"
+            :label="$t('team_type')"
+            id="team type"
+            :placeholder="$t('team_type')"
+            @update:modelValue="setEquipments"
+          />
+        </div>
+        <div class="input-wrapper">
+          <CustomSelectInput
+            :modelValue="Device"
+            :static-options="Devices"
+            class="input"
+            :label="$t('employee')"
+            id="employee"
+            :placeholder="$t('employee')"
+            @update:modelValue="setDevices"
+          />
+        </div>
+      </div>
+
+      <div class="submit-btn">
+        <button class="btn btn-ptimary">confirm</button>
+      </div>
+    </form>
+  </div>
+</template>
