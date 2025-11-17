ebb814d46e4af1435bf2a8ad5dd710f7e1c8be1c
MohamedTarek1993 <www.mtarek789@gmail.com>
2025-11-17 09:12:23 +0200

    equipment details

---
+++ src/features/setting/Equipment/Presentation/components/details/CardEquipment.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card-equipment">
+    <img src="@/assets/images/Rectangle 39933.png" alt="" class="img-equipment" />
+    <div class="card-body">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quaerat cumque. Ratione cumque
+      nam vero quasi doloribus corporis facere accusamus eius dolore aperiam ducimus eligendi minus
+      odio voluptatibus, quae earum!
+    </div>
+  </div>
+</template>
--- src/features/setting/Equipment/Presentation/components/details/ShowEquipment.vue
+++ src/features/setting/Equipment/Presentation/components/details/ShowEquipment.vue
@@ -1,11 +1,9 @@
-<template>
-  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, omnis, numquam possimus repellat
-  rem dignissimos minima consequuntur quis magni aliquid maiores tempore impedit suscipit delectus
-  consectetur earum illo nulla ratione.
-</template>
-
-<script>
-export default {}
+<script setup lang="ts">
+import CardEquipment from './CardEquipment.vue'
 </script>
 
-<style></style>
+<template>
+  <div class="show-equipment">
+    <CardEquipment />
+  </div>
+</template>
