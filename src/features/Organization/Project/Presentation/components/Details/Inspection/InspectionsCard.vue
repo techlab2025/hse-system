31c4e50da9aa3f0ea92ff4922d7528da28138369
MohamedTarek1993 <www.mtarek789@gmail.com>
2025-11-23 10:46:51 +0200

    fix Observation imports

---
+++ src/features/Organization/Project/Presentation/components/Details/Inspection/InspectionsCard.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import PinIcons from '@/shared/icons/PinIcons.vue'
+</script>
+
+<template>
+  <div class="inspections-card">
+    <div   class="card-header">
+      <div class="card-header-title">
+        <PinIcons />
+        <p class="title">Obour City</p>
+      </div>
+      <div class="tasks-num">10 {{ $t('tasks') }}</div>
+    </div>
+  </div>
+</template>
