6bb20a9165959bfd68e372d2ce918b0c47ccc75a
MohabMohamed <mohabmohamedd772@gmail.com>
2025-11-23 17:13:06 +0200

    update index

--- src/features/Organization/Inspection/Presentation/components/InspectionDialog/TemplateDocumentTypes/TemplateDocumentCheckboxShow.vue
+++ src/features/Organization/Inspection/Presentation/components/InspectionDialog/TemplateDocumentTypes/TemplateDocumentCheckboxShow.vue
@@ -4,37 +4,39 @@ import UploadImage from '@/shared/icons/UploadImage.vue'
 import type ItemModel from '@/features/setting/TemplateItem/Data/models/ItemMode'
 import UploadMultiImage from '@/shared/HelpersComponents/UploadMultiImage.vue'
 import { ref } from 'vue'
-const emit = defineEmits(['update:data'])
-const { title, options } = defineProps<{
+const emit = defineEmits(['update:data', 'update:images'])
+
+const { title, options, require_image } = defineProps<{
   title: string
   options: ItemModel[]
   require_image: boolean
 }>()
 
-const SelectedOptions = ref<{ optionTitle: string, isSelected: boolean[] }[]>([])
-const UpdateData = (title: string, id: number) => {
-  SelectedOptions.value.push({ optionTitle: title, isSelected: [id] })
-  console.log(SelectedOptions.value, 'SelectedOptions.value')
-  emit('update:data')
+// list of selected IDs
+const SelectedOptions = ref<number[]>([])
+
+const UpdateData = () => {
+  emit('update:data', {
+    title,
+    selected: SelectedOptions.value
+  })
 }
 </script>
 <template>
   <div class="show-template-document-checkbox">
     <p class="title">{{ title }}</p>
+
     <div class="options-container">
       <div class="options">
         <div class="options-box" v-for="(option, index) in options" :key="index">
           <label :for="`checkbox-${index}-${title}`" class="label">{{ option.title }}</label>
-          <Checkbox binary :inputId="`checkbox-${index}-${title}`" type="checkbox" class="input"
-            @change="UpdateData(title, option.id)" name="checkbox-option" />
+
+          <Checkbox binary :value="option.id" v-model="SelectedOptions" @change="UpdateData"
+            :inputId="`checkbox-${index}-${title}`" :name="`${title}-${option.id}`" />
         </div>
       </div>
 
-      <UploadMultiImage
-        class="image-upload"
-        v-if="require_image"
-        @update:images="$emit('update:images', $event)"
-      />
+      <UploadMultiImage v-if="require_image" @update:images="emit('update:images', $event)" />
     </div>
   </div>
 </template>
