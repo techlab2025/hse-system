0d6974627bc289a78bd16ad2f534fd90e6cb44d9
Sayed2210 <eh600039@gmail.com>
2025-12-04 14:20:03 +0200

    filter invoices

--- src/features/Organization/ObservationFactory/Presentation/components/Ovservation/AllObservation.vue
+++ src/features/Organization/ObservationFactory/Presentation/components/Ovservation/AllObservation.vue
@@ -2,34 +2,38 @@
 import HeaderPage from '@/features/Organization/Project/Presentation/components/Details/DetailsHeader/HeaderPage.vue'
 import allObservationImage from '@/assets/images/services 1.png'
 import ObservationCard from './cards/ObservationCard.vue'
+import observationImage  from "@/assets/images/thinking.png"
+import hazardImage  from "@/assets/images/hazard-sign.png"
+import incedantImage  from "@/assets/images/risk-assessment-document.png"
+import inspectionImage  from "@/assets/images/inspection.png"
 
 
 const items = [
   {
     title: 'Observation',
     addRoute: 'equipment/observation/add',
-    image: '/src/assets/images/thinking.png',
+    image: observationImage,
     indexRoute: '/organization/equipment/observation',
     description: 'You can Show all observations here'
   },
   {
     title: 'Hazard',
     addRoute: 'equipment/hazard/add',
-    image: '/src/assets/images/hazard-sign.png',
+    image: hazardImage,
     indexRoute: '/organization/equipment/hazard',
     description: 'You can Show all hazards here'
   },
   {
     title: 'Incident',
     addRoute: 'equipment/incedant/add',
-    image: '/src/assets/images/risk-assessment-document.png',
+    image: incedantImage,
     indexRoute: '/organization/equipment/incedant',
     description: 'You can Show all incidents here'
   },
   {
     title: 'Inspection',
     addRoute: 'equipment/inspection/add',
-    image: '/src/assets/images/inspection.png',
+    image: inspectionImage,
     indexRoute: '/organization/equipment/inspection',
     description: 'You can Show all inspections here'
   }
