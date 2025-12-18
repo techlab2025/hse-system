19a0b91965b898e7134618049f1a84d47ea13078
MohabMohamed <mohabmohamedd772@gmail.com>
2025-12-18 17:45:40 +0200

    update project deails

--- src/features/Organization/Project/Core/params/Hierarchy/HierarchyEmployee/addHierarchyEmployeeParams.ts
+++ src/features/Organization/Project/Core/params/Hierarchy/HierarchyEmployee/addHierarchyEmployeeParams.ts
@@ -1,24 +1,19 @@
 import type Params from '@/base/core/params/params'
-import type LocationHierarchyEmployeeParams from './locationHierarchyEmployeeParams';
+import type LocationHierarchyEmployeeParams from './locationHierarchyEmployeeParams'
 
 export default class AddHierarchyEmployeeParams implements Params {
   projectId: number
   locations: LocationHierarchyEmployeeParams[]
 
-  constructor(
-    projectId: number,
-    locations: LocationHierarchyEmployeeParams[]
-  ) {
+  constructor(projectId: number, locations: LocationHierarchyEmployeeParams[]) {
     this.projectId = projectId
     this.locations = locations
   }
 
   toMap(): Record<string, unknown> {
     const data: Record<string, unknown> = {}
-
-    data['project_id'] = this.projectId
-    data['locations'] = this.locations
-
+    if (this.locations != null) data['project_id'] = this.projectId
+    if (this.locations != null) data['locations'] = this.locations
     return data
   }
 }
