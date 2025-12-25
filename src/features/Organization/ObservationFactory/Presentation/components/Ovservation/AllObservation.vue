20d7dd650c5b5020e867c39a9a79281a7041cdf6
MohabMohamed <mohabmohamedd772@gmail.com>
2025-12-25 16:37:15 +0200

    update observation operation

--- src/features/Organization/ObservationFactory/Presentation/components/Ovservation/AllObservation.vue
+++ src/features/Organization/ObservationFactory/Presentation/components/Ovservation/AllObservation.vue
@@ -9,33 +9,35 @@ import ToDoLogo from "@/assets/images/ToDoLogo.png"
 
 const items = [
   {
+    title: 'Inspection',
+    image: ToDoLogo,
+    addRoute: '/organization/equipment-mangement/inspection/add',
+    indexRoute: '/organization/equipment-mangement/inspection?isAll=1',
+    description: 'Add . show'
+  },
+  {
     title: 'Observation',
     image: Political,
     addRoute: '/organization/equipment-mangement/observation/add',
     indexRoute: '/organization/equipment-mangement/observation?isAll=1',
-    description: 'Create . show'
-  },
-  {
-    title: 'Hazard',
-    image: ALERT,
-    addRoute: '/organization/equipment-mangement/hazard/add',
-    indexRoute: '/organization/equipment-mangement/hazard?isAll=1',
-    description: 'Create . show'
+    description: 'Add . show'
   },
   {
     title: 'Incident',
     image: PoliceLine,
     addRoute: '/organization/equipment-mangement/incedant/add',
     indexRoute: '/organization/equipment-mangement/incedant?isAll=1',
-    description: 'Create . show'
+    description: 'Add . show'
   },
   {
-    title: 'Inspection',
-    image: ToDoLogo,
-    addRoute: '/organization/equipment-mangement/inspection/add',
-    indexRoute: '/organization/equipment-mangement/inspection?isAll=1',
-    description: 'Create . show'
-  }
+    title: 'Hazard',
+    image: ALERT,
+    addRoute: '/organization/equipment-mangement/hazard/add',
+    indexRoute: '/organization/equipment-mangement/hazard?isAll=1',
+    description: 'Add . show'
+  },
+
+
 ]
 </script>
 
