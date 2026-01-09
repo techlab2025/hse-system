83c68103e9cbbd4c41afb0cd97c4785820d0c0f7
MohabMohamed72 <bhoba17@gmail.com>
2026-01-09 20:04:16 +0200

    add project card

---
+++ src/features/Organization/Project/Presentation/components/ProjectUtils/ProjectCard.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import type ProjectModel from '../../../Data/models/ProjectModel';
+
+const props = defineProps<{
+  data: ProjectModel
+}>()
+</script>
+<template>
+  <div class="project-card-container">
+
+  </div>
+</template>
