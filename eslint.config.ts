5613e36eca2fbd15ea40fdb0248d34408f04f527
Sayed2210 <eh600039@gmail.com>
2026-01-19 11:49:53 +0200

    AUTO REMOVE unused imports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,17 +1,35 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
+import unusedImports from 'eslint-plugin-unused-imports'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
-// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
-// import { configureVueProject } from '@vue/eslint-config-typescript'
-// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
-// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    plugins: {
+      vue: pluginVue,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      // 🚨 AUTO REMOVE unused imports
+      'unused-imports/no-unused-imports': 'error',
+
+      // ⚠️ Warn unused vars (allow _)
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      // Required for <script setup>
+      'vue/script-setup-uses-vars': 'error',
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
