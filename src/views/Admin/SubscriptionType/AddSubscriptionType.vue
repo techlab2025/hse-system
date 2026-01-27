1350dd73f7adbc32d41901af28626b072e5c2ef7
MohabMohamed <mohabmohamedd772@gmail.com>
2026-01-27 17:31:27 +0200

    subscription type

--- src/views/Admin/SubscriptionType/AddSubscriptionType.vue
+++ src/views/Admin/SubscriptionType/AddSubscriptionType.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import AddSubscriptionType from '@/features/setting/SubscriptionType/Presentation/components/AddSubscriptionType.vue'
 </script>
 
 <template>
+  <!-- adasd -->
   <AddSubscriptionType />
 </template>
