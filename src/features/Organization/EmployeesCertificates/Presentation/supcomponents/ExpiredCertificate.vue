eb6a6df55d90f7c66fd76966189d6ae0f9f163c4
Min3m <m.01002671365@gmail.com>
2026-02-01 14:32:33 +0200

    Certificates Uploaing

--- src/features/Organization/EmployeesCertificates/Presentation/supcomponents/ExpiredCertificate.vue
+++ src/features/Organization/EmployeesCertificates/Presentation/supcomponents/ExpiredCertificate.vue
@@ -1,27 +1,34 @@
 <script lang="ts" setup>
-import CertificateImageDialog from "./CertificateImageDialog.vue"
-import ExpiredIcon from '@/shared/icons/ExpiredIcon.vue';
+import { CertificateStatusEnum } from '@/features/Organization/OrganizationEmployee/Core/Enum/CertificateStatusEnum'
+import CertificateImageDialog from './CertificateImageDialog.vue'
+import ExpiredIcon from '@/shared/icons/ExpiredIcon.vue'
 
 const props = defineProps<{
-  certificateId: number,
-  organizationEmployeeId: number,
-  is_expire_date: boolean,
+  certificateId: number
+  organizationEmployeeId: number
+  is_expire_date: boolean
+  status: CertificateStatusEnum
 }>()
 
 const emit = defineEmits(['update:data'])
 const updateData = () => {
   emit('update:data')
 }
-
 </script>
 <template>
   <div class="expired-certificate">
     <div class="expired">
-      <ExpiredIcon />
-      <span class="expired-title">Expired</span>
+      <!-- <ExpiredIcon /> -->
+      <span class="expired-title">Expired </span>
     </div>
     <!-- <p class="expired-date">Renew</p> -->
-    <CertificateImageDialog :title="`Renew`" @update:data="updateData" :certificateId="certificateId"
-      :organizationEmployeeId="organizationEmployeeId" :is_expire_date="is_expire_date" />
+    <CertificateImageDialog
+      :title="`Renew`"
+      @update:data="updateData"
+      :certificateId="certificateId"
+      :organizationEmployeeId="organizationEmployeeId"
+      :is_expire_date="is_expire_date"
+      :status="status"
+    />
   </div>
 </template>
