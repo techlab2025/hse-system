711f92c9ff9a015fb494bbc57872dd45daf28c17
MohabMohamed <mohabmohamedd772@gmail.com>
2026-02-03 14:13:24 +0200

    add ticket in home

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
 import LoaderDialog from './base/Presentation/Dialogs/LoaderDialogs/LoaderDialog.vue'
 import MainDialog from './base/Presentation/Dialogs/MainDialogs/MainDialog.vue'
 import Toast from 'primevue/toast'
+import TicketIcon from './shared/icons/TicketIcon.vue';
+import { ref } from 'vue';
+import { useTicketStore } from './stores/TicketStor';
+import TicketDialog from './features/Organization/Ticket/Presentation/components/TicketDialog/TicketDialog.vue';
 
 
-
+const ticketStore = useTicketStore()
 
 </script>
 
@@ -13,4 +17,6 @@ import Toast from 'primevue/toast'
   <MainDialog />
   <LoaderDialog />
   <RouterView />
+  <TicketIcon class="ticket-icon" @click="ticketStore.captureScreen" />
+  <TicketDialog />
 </template>
