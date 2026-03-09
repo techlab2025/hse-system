3c05fcd7627f5cbece41757432b5925d78cda99a
MohabMohamed <mohabmohamedd772@gmail.com>
2026-03-09 12:55:24 +0200

    update

--- src/features/Organization/OrganizationEmployee/Core/params/indexOrganizatoinEmployeeParams.ts
+++ src/features/Organization/OrganizationEmployee/Core/params/indexOrganizatoinEmployeeParams.ts
@@ -10,6 +10,9 @@ export default class IndexOrganizatoinEmployeeParams implements Params {
   public heirarchyId?: number
   public projectZoneId?: number
   public returndeadonly?: number
+  public CertificateId?: number
+  public shouldHaveThisCertificate?: boolean
+  public shouldNotHaveThisHierarchy?: boolean
 
   constructor(
     word: string,
@@ -19,6 +22,9 @@ export default class IndexOrganizatoinEmployeeParams implements Params {
     heirarchyId?: number,
     projectZoneId?: number,
     returndeadonly?: number,
+    CertificateId?: number,
+    shouldHaveThisCertificate?: boolean,
+    shouldNotHaveThisHierarchy?: boolean,
     // code?: LangEnum,
   ) {
     this.word = word
@@ -28,19 +34,29 @@ export default class IndexOrganizatoinEmployeeParams implements Params {
     this.heirarchyId = heirarchyId
     this.projectZoneId = projectZoneId
     this.returndeadonly = returndeadonly
+    this.CertificateId = CertificateId
+    this.shouldHaveThisCertificate = shouldHaveThisCertificate
+    this.shouldNotHaveThisHierarchy = shouldNotHaveThisHierarchy
     // this.code = code
   }
 
   toMap(): Record<string, string | number | number[] | null> {
-    const data: Record<string, string | number | number[] | null> = {}
+    const data: Record<string, string | number | number[] | null | any> = {}
     if (this.word) data['word'] = this.word
     data['paginate'] = this.withPage
     data['page'] = this.pageNumber
     data['limit'] = this.perPage
-    if (this.heirarchyId) data['hierarchy_id'] = Number(this.heirarchyId)
-    if (this.projectZoneId) data['project_zone_id'] = Number(this.projectZoneId)
-    if (this.returndeadonly || this.returndeadonly == false)
+    if (this.heirarchyId != null) data['hierarchy_id'] = Number(this.heirarchyId)
+    if (this.projectZoneId != null) data['project_zone_id'] = Number(this.projectZoneId)
+    if (this.returndeadonly != null || this.returndeadonly == false)
       data['not_include_dead_employees'] = this.returndeadonly
+    if (this.CertificateId != null) data['certificate_id'] = Number(this.CertificateId)
+
+    if (this.shouldHaveThisCertificate != null || this.shouldHaveThisCertificate == false)
+      data['should_have_this_certificate'] = this.shouldHaveThisCertificate
+
+    if (this.shouldNotHaveThisHierarchy != null || this.shouldNotHaveThisHierarchy == false)
+      data['should_not_have_this_hierarchy'] = this.shouldNotHaveThisHierarchy
 
     // if (this.code) data['code'] = this.code
     return data
