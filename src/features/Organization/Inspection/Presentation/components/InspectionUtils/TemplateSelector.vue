e66353ce51f4c3e6c24d18541698ce486cf37c7d
mahmoudaly66 <mahmoud666aly@gmail.com>
2026-03-15 11:16:15 +0200

    remove accordion

--- src/features/Organization/Inspection/Presentation/components/InspectionUtils/TemplateSelector.vue
+++ src/features/Organization/Inspection/Presentation/components/InspectionUtils/TemplateSelector.vue
@@ -1,8 +1,4 @@
 <script setup lang="ts">
-import Accordion from 'primevue/accordion';
-import AccordionPanel from 'primevue/accordionpanel';
-import AccordionHeader from 'primevue/accordionheader';
-import AccordionContent from 'primevue/accordioncontent';
 import TemplateDocument from '@/features/setting/TemplateItem/Presentation/components/TemplateDocument.vue'
 import type TemplateModel from '@/features/setting/Template/Data/models/TemplateModel';
 import DocumnetHeader from '@/assets/images/DocumnetHeader.png'
@@ -17,7 +13,7 @@ const props = defineProps<{
   selectedTemplates?: number
 }>()
 
-const SelectedTemplate = ref<number>(props.selectedTemplates)
+const SelectedTemplate = ref<number | undefined>(props.selectedTemplates)
 const SetSelectedTemplate = (id: number) => {
   SelectedTemplate.value = id
   emit('update:data', id)
@@ -28,65 +24,54 @@ watch(() => props.selectedTemplates, (newVal) => {
 })
 
 const showMore = ref(false)
-const activeaccordion = ref<string | null>(null)
-
-const toggleAccordion = (id: string) => {
-  activeaccordion.value = activeaccordion.value === id ? null : id
-}
 </script>
 
 <template>
   <div class="template-selector">
-    <Accordion value="0" :multiple="false">
-      <div class="template-selector-container-AccordionPanel">
-        <AccordionPanel @click="SetSelectedTemplate(template.id)" class="panel-acc"
-          :class="SelectedTemplate == template.id ? 'active' : ''" value="0" v-for="(template, index) in data"
-          :key="index">
-          <AccordionHeader @click.prevent="toggleAccordion(String(template.id))">
-            <div class="template-document-container">
-              <div class="template-document-header">
-                <div class="template-header">
-                  <p class="header-title" v-if="template.titles && template?.titles?.length > 0">
-                    {{
-                      template?.titles
-                        ?.filter((item) => item.locale === 'en')
-                        .map((item) => item.title)
-                        .join('')
-                    }}
-                  </p>
-                  <p class="header-title" v-else>{{ wordSlice(template?.title, 20) }}</p>
-                  <div class="template-details">
-                    <p>
-                      {{ $t('id') }} : <span>{{ template?.id }}</span>
-                    </p>
-                  </div>
-                </div>
-                <div class="header-select">
-                  <div class="template-checkbox">
-                    <RadioButton :inputId="`${template.id}`" name="template" v-model="SelectedTemplate"
-                      :value="template.id" />
-                    <label :for="`${template.id}`">{{ $t('select') }}</label>
-                  </div>
-                  <img :src="DocumnetHeader" alt="header" />
-                </div>
+    <div class="template-selector-container-AccordionPanel">
+      <div @click="SetSelectedTemplate(template.id)" class="panel-acc"
+        :class="SelectedTemplate == template.id ? 'active' : ''" v-for="(template, index) in data"
+        :key="index">
+        <div class="template-document-container">
+          <div class="template-document-header">
+            <div class="template-header">
+              <p class="header-title" v-if="template.titles && template?.titles?.length > 0">
+                {{
+                  template?.titles
+                    ?.filter((item) => item.locale === 'en')
+                    .map((item) => item.title)
+                    .join('')
+                }}
+              </p>
+              <p class="header-title" v-else>{{ wordSlice(template?.title, 20) }}</p>
+              <div class="template-details">
+                <p>
+                  {{ $t('id') }} : <span>{{ template?.id }}</span>
+                </p>
               </div>
             </div>
-          </AccordionHeader>
-          <AccordionContent>
-            <TemplateDocument :isActions="false" :allData="template" :headerDisplay="false" />
-          </AccordionContent>
-        </AccordionPanel>
+            <div class="header-select">
+              <div class="template-checkbox">
+                <RadioButton :inputId="`${template.id}`" name="template" v-model="SelectedTemplate"
+                  :value="template.id" />
+                <label :for="`${template.id}`">{{ $t('select') }}</label>
+              </div>
+              <img :src="DocumnetHeader" alt="header" />
+            </div>
+          </div>
+        </div>
+        <div class="template-document-content">
+          <TemplateDocument :isActions="false" :allData="template" :headerDisplay="false" />
+        </div>
       </div>
+    </div>
 
-      <!-- <button class="show-more-btn" v-if="data.length > 2" @click="showMore = !showMore">
-        <span>{{ showMore ? 'Show Less' : 'Show More' }}</span>
-        <IconArrowDownNav :class="showMore ? 'rotate' : ''" />
-      </button> -->
-    </Accordion>
+    <!-- <button class="show-more-btn" v-if="data.length > 2" @click="showMore = !showMore">
+      <span>{{ showMore ? 'Show Less' : 'Show More' }}</span>
+      <IconArrowDownNav :class="showMore ? 'rotate' : ''" />
+    </button> -->
   </div>
 </template>
-
-
 
 <style lang="scss" scoped>
 .template-selector-container-AccordionPanel .active.panel-acc{
