9604e31bb71cd5c2c653b9c0d86216f563d1e3d5
mahmoudaly66 <mahmoud666aly@gmail.com>
2026-03-16 15:02:50 +0200

    capa card

--- src/features/Organization/ObservationFactory/Data/models/CapaModel.ts
+++ src/features/Organization/ObservationFactory/Data/models/CapaModel.ts
@@ -1,15 +1,17 @@
 export default class CapaModel {
   public preventive: string
   public corrective: string
+  public serial_name:Number
 
-  constructor(preventive: string, corrective: string) {
+  constructor(preventive: string, corrective: string, serial_name:Number) {
     this.preventive = preventive
     this.corrective = corrective
+    this.serial_name = serial_name
   }
 
   static fromMap(data: any): CapaModel {
-    return new CapaModel(data.preventive, data.corrective)
+    return new CapaModel(data.preventive, data.corrective, data.serial_name)
   }
 
-  static example: CapaModel = new CapaModel('preventive', 'corrective')
+  static example: CapaModel = new CapaModel('preventive', 'corrective', 1)
 }
