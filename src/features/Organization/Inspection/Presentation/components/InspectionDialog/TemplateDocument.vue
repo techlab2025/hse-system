4b2cc0e74b4bc6d9b07a34777a2a7cdf4f070b18
Mohab <mohabmohamedd772@gmail.com>
2026-07-13 12:13:11 +0300

    upfdte notification

--- src/features/Organization/Inspection/Presentation/components/InspectionDialog/TemplateDocument.vue
+++ src/features/Organization/Inspection/Presentation/components/InspectionDialog/TemplateDocument.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import DocumnetHeader from '@/assets/images/DocumnetHeader.png'
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import type TemplateDetailsModel from '@/features/setting/Template/Data/models/TemplateDetailsModel'
 import TemplateDocumentCheckboxShow from './TemplateDocumentTypes/TemplateDocumentCheckboxShow.vue'
 import TemplateDocumentRadioButtonShow from './TemplateDocumentTypes/TemplateDocumentRadioButtonShow.vue'
@@ -8,6 +8,7 @@ import TemplateDocumentSelectShow from './TemplateDocumentTypes/TemplateDocument
 import TemplateDocumentTextAreaShow from './TemplateDocumentTypes/TemplateDocumentTextAreaShow.vue'
 import { ActionsEnum } from '@/features/setting/TemplateItem/Core/Enum/ActionsEnum'
 import type TaskResultModel from '../../../Data/models/FetchTaskResultModels/TasksResultModel'
+import type TaskResultItemModel from '../../../Data/models/FetchTaskResultModels/ItemTasksResultModel'
 import TemplateDocumentSelectShowResult from '../ShowResultActions/TemplateDocumentSelectShowResult.vue'
 import TemplateDocumentCheckboxShowResult from '../ShowResultActions/TemplateDocumentCheckboxShowResult.vue'
 import TemplateDocumentRadioShowResult from '../ShowResultActions/TemplateDocumentRadioShowResult.vue'
@@ -133,34 +134,45 @@ const UpdateData = () => {
   })
 }
 
-const TaskAnswerMap = ref<Map<number, any>>(new Map())
+const hasTaskResults = computed(() => (props.task_results?.taskResultItems?.length ?? 0) > 0)
 
-watch(
-  () => props.task_results,
-  (newState) => {
-    if (newState && newState.taskResultItems) {
-      // Clear the map
-      TaskAnswerMap.value.clear()
-      newState.taskResultItems.forEach((item) => {
-        TaskAnswerMap.value.set(item.id, item)
-      })
-    }
-  },
-)
+const hasAnsweredResult = (resultItem?: TaskResultItemModel) => {
+  if (!resultItem) return false
 
-// Helper function to get the answer for a specific template item
-const getAnswerForItem = (templateItemId: number) => {
-  if (!props.task_results?.taskResultItems) return null
-  const resultItem = props.task_results.taskResultItems.find((item) => {
-    return true // Placeholder
+  const hasAnswer = resultItem.answers?.some((answer) => {
+    const textAnswer = String(answer?.answer ?? '').trim()
+    const option = answer?.templateItemOption
+
+    return Boolean(textAnswer || option?.id || option?.title)
   })
 
-  return resultItem
+  return Boolean(hasAnswer || resultItem.files?.length > 0 || String(resultItem.result ?? '').trim())
 }
+
+const getAnsweredResultForItem = (templateItemId: number, action: number) => {
+  const resultItem = props.task_results?.taskResultItems?.find(
+    (item) => item.templateItemId === templateItemId && item.templateItemAction === action,
+  )
+
+  return hasAnsweredResult(resultItem) ? resultItem : undefined
+}
+
+const visibleTemplateItemTags = computed(() => {
+  const tags = props.allData?.templateItemTags ?? []
+  if (!hasTaskResults.value) return tags
+
+  return tags
+    .map((tag) => ({
+      ...tag,
+      templateItems: tag.templateItems.filter((item) =>
+        getAnsweredResultForItem(item.id, item.action),
+      ),
+    }))
+    .filter((tag) => tag.templateItems.length > 0)
+})
 </script>
 <template>
   <!-- <pre>{{ task_results }}</pre> -->
-  
   <div class="template-document-container w-full">
     <div class="template-document-header">
       <div class="template-header">
@@ -186,7 +198,7 @@ const getAnswerForItem = (templateItemId: number) => {
       <img :src="DocumnetHeader" alt="header" />
     </div>
     <div class="template-document-content-container w-full">
-      <div v-for="(tag, index) in allData?.templateItemTags" :key="index">
+      <div v-for="(tag, index) in visibleTemplateItemTags" :key="index">
         <div>
           <p class="tag-title" v-if="tag.titles?.length > 0">
             {{
@@ -205,7 +217,6 @@ const getAnswerForItem = (templateItemId: number) => {
           :key="index"
         >
           <!-- <div class="overlay" v-if="isOverlay"></div> -->
-
           <div class="w-full" v-if="item?.action == ActionsEnum.CHECKBOX">
             <div class="w-full" v-if="task_results?.taskResultItems?.length > 0">
               <TemplateDocumentCheckboxShowResult
@@ -215,15 +226,7 @@ const getAnswerForItem = (templateItemId: number) => {
                 :options="item.options"
                 :require_image="item.requiredImage"
                 @update:data="UpdateCheckBoxs"
-                :selected_data="
-                  task_results?.taskResultItems
-                    ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.CHECKBOX)
-                    .find(
-                      (resultItem, idx) =>
-                        tag.templateItems.filter((ti) => ti.action === ActionsEnum.CHECKBOX)[idx]
-                          ?.id === item.id,
-                    )
-                "
+                :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.CHECKBOX)"
               />
             </div>
             <TemplateDocumentCheckboxShow
@@ -234,15 +237,7 @@ const getAnswerForItem = (templateItemId: number) => {
               :options="item.options"
               :require_image="item.requiredImage"
               @update:data="UpdateCheckBoxs"
-              :selected_data="
-                task_results?.taskResultItems
-                  ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.CHECKBOX)
-                  .find(
-                    (resultItem, idx) =>
-                      tag.templateItems.filter((ti) => ti.action === ActionsEnum.CHECKBOX)[idx]
-                        ?.id === item.id,
-                  )
-              "
+              :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.CHECKBOX)"
             />
           </div>
 
@@ -254,17 +249,7 @@ const getAnswerForItem = (templateItemId: number) => {
                 :options="item.options"
                 :require_image="item.requiredImage"
                 @update:data="UpdateRadioButtons"
-                :selected_data="
-                  task_results?.taskResultItems
-                    ?.filter(
-                      (resultItem) => resultItem.templateItemAction === ActionsEnum.RADIOBUTTON,
-                    )
-                    .find(
-                      (resultItem, idx) =>
-                        tag.templateItems.filter((ti) => ti.action === ActionsEnum.RADIOBUTTON)[idx]
-                          ?.id === item.id,
-                    )
-                "
+                :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.RADIOBUTTON)"
               />
             </div>
             <TemplateDocumentRadioButtonShow
@@ -274,17 +259,7 @@ const getAnswerForItem = (templateItemId: number) => {
               :options="item.options"
               :require_image="item.requiredImage"
               @update:data="UpdateRadioButtons"
-              :selected_data="
-                task_results?.taskResultItems
-                  ?.filter(
-                    (resultItem) => resultItem.templateItemAction === ActionsEnum.RADIOBUTTON,
-                  )
-                  .find(
-                    (resultItem, idx) =>
-                      tag.templateItems.filter((ti) => ti.action === ActionsEnum.RADIOBUTTON)[idx]
-                        ?.id === item.id,
-                  )
-              "
+              :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.RADIOBUTTON)"
             />
           </div>
 
@@ -297,15 +272,7 @@ const getAnswerForItem = (templateItemId: number) => {
                 :options="item.options"
                 :require_image="item.requiredImage"
                 @update:data="UpdateSelects"
-                :selected_data="
-                  task_results?.taskResultItems
-                    ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.DROPDOWN)
-                    .find(
-                      (resultItem, idx) =>
-                        tag.templateItems.filter((ti) => ti.action === ActionsEnum.DROPDOWN)[idx]
-                          ?.id === item.id,
-                    )
-                "
+                :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.DROPDOWN)"
               />
             </div>
             <TemplateDocumentSelectShow
@@ -316,15 +283,7 @@ const getAnswerForItem = (templateItemId: number) => {
               :options="item.options"
               :require_image="item.requiredImage"
               @update:data="UpdateSelects"
-              :selected_data="
-                task_results?.taskResultItems
-                  ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.DROPDOWN)
-                  .find(
-                    (resultItem, idx) =>
-                      tag.templateItems.filter((ti) => ti.action === ActionsEnum.DROPDOWN)[idx]
-                        ?.id === item.id,
-                  )
-              "
+              :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.DROPDOWN)"
             />
           </div>
           <div class="w-full" v-if="item?.action == ActionsEnum.TEXTAREA">
@@ -334,15 +293,7 @@ const getAnswerForItem = (templateItemId: number) => {
                 :item_id="item.id"
                 :require_image="item.requiredImage"
                 @update:data="UpdateTextAreas"
-                :selected_data="
-                  task_results?.taskResultItems
-                    ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.TEXTAREA)
-                    .find(
-                      (resultItem, idx) =>
-                        tag.templateItems.filter((ti) => ti.action === ActionsEnum.TEXTAREA)[idx]
-                          ?.id === item.id,
-                    )
-                "
+                :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.TEXTAREA)"
               />
             </div>
             <TemplateDocumentTextAreaShow
@@ -351,15 +302,7 @@ const getAnswerForItem = (templateItemId: number) => {
               :item_id="item.id"
               :require_image="item.requiredImage"
               @update:data="UpdateTextAreas"
-              :selected_data="
-                task_results?.taskResultItems
-                  ?.filter((resultItem) => resultItem.templateItemAction === ActionsEnum.TEXTAREA)
-                  .find(
-                    (resultItem, idx) =>
-                      tag.templateItems.filter((ti) => ti.action === ActionsEnum.TEXTAREA)[idx]
-                        ?.id === item.id,
-                  )
-              "
+              :selected_data="getAnsweredResultForItem(item.id, ActionsEnum.TEXTAREA)"
             />
           </div>
         </div>
