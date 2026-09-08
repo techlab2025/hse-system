5827c2ce88f8d3abfac831dad9c9ca0efa0bf693
Mohab <mohabmohamedd772@gmail.com>
2026-09-08 14:43:01 +0300

    udpate project steps

---
+++ src/features/Organization/Project/Data/apiServices/UpdatedProjectFlow/ProjectFlowApiServices.ts
@@ -0,0 +1,71 @@
+import { ApiNames } from '@/base/core/networkStructure/apiNames'
+import ServicesInterface from '@/base/Data/ApiService/api_service_interface'
+import { CrudType } from '@/base/core/params/call_params_interface'
+import type Params from '@/base/core/params/params'
+import HeaderHandler from '@/base/core/networkStructure/networking/utils/header_handler'
+import type {
+  BasicProjectParams,
+  ProjectEquipmentsParams,
+  ProjectHolidaysParams,
+  ProjectLocationPositionEmployeesParams,
+  ProjectTeamsParams,
+} from '../../../Core/params/UpdatedProjectFlow/ProjectFlowParams'
+
+abstract class ProjectFlowApiService extends ServicesInterface {
+  protected abstract createUrl: string
+  protected abstract updateUrl: string
+
+  async applyService(params: Params) {
+    const projectId = (params as { projectId?: number }).projectId
+    return super.call({
+      url: projectId ? this.updateUrl : this.createUrl,
+      type: CrudType.POST,
+      auth: true,
+      params,
+      showLoadingDialog: false,
+    })
+  }
+}
+
+export class BasicProjectApiService extends ProjectFlowApiService {
+  protected get createUrl() { return ApiNames.instance.CreateBasicProject }
+  protected get updateUrl() { return ApiNames.instance.UpdateBasicProject }
+  applyService(params: BasicProjectParams) { return super.applyService(params) }
+}
+
+export class ProjectHolidaysApiService extends ProjectFlowApiService {
+  protected get createUrl() { return ApiNames.instance.CreateProjectHolidays }
+  protected get updateUrl() { return ApiNames.instance.UpdateProjectHolidays }
+  applyService(params: ProjectHolidaysParams) { return super.applyService(params) }
+}
+
+export class ProjectLocationPositionEmployeesApiService extends ProjectFlowApiService {
+  protected get createUrl() { return ApiNames.instance.CreateProjectLocationPositionEmployees }
+  protected get updateUrl() { return ApiNames.instance.UpdateProjectLocationPositionEmployees }
+  applyService(params: ProjectLocationPositionEmployeesParams) { return super.applyService(params) }
+}
+
+export class ProjectTeamsApiService extends ProjectFlowApiService {
+  protected get createUrl() { return ApiNames.instance.CreateProjectTeams }
+  protected get updateUrl() { return ApiNames.instance.UpdateProjectTeams }
+  applyService(params: ProjectTeamsParams) { return super.applyService(params) }
+}
+
+export class ProjectEquipmentsApiService extends ProjectFlowApiService {
+  protected get createUrl() { return ApiNames.instance.CreateProjectEquipments }
+  protected get updateUrl() { return ApiNames.instance.UpdateProjectEquipments }
+  applyService(params: ProjectEquipmentsParams) { return super.applyService(params) }
+}
+
+export class ProjectFlowDetailsApiService extends ServicesInterface {
+  applyService(params: Params) {
+    return super.call({
+      url: ApiNames.instance.ShowProject,
+      type: CrudType.FormData,
+      auth: true,
+      params,
+      headers: HeaderHandler.Instance.getHeader(true, false),
+      showLoadingDialog: false,
+    })
+  }
+}
