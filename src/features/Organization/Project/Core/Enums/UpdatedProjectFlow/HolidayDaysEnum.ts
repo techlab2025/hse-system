22a2ced20a656df3e1ba9a3a0eaa359853e8d1e2
Mohab <mohabmohamedd772@gmail.com>
2026-09-09 15:09:23 +0300

    update days enum

--- src/features/Organization/Project/Core/Enums/UpdatedProjectFlow/HolidayDaysEnum.ts
+++ src/features/Organization/Project/Core/Enums/UpdatedProjectFlow/HolidayDaysEnum.ts
@@ -1,10 +1,9 @@
 export enum HolidayDaysEnum {
-  SUNDAY = 1,
-  MONDAY = 2,
-  TUESDAY = 3,
-  WEDNESDAY = 4,
-  THURSDAY = 5,
-  FRIDAY = 6,
-  SATURDAY = 7,
+  SUNDAY = 0,
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6,
 }
-
