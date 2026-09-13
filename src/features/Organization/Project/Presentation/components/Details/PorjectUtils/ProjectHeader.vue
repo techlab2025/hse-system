88635203f77081d515365985621c606974f935ab
Mohab <mohabmohamedd772@gmail.com>
2026-09-13 12:37:56 +0300

    update driil

--- src/features/Organization/Project/Presentation/components/Details/PorjectUtils/ProjectHeader.vue
+++ src/features/Organization/Project/Presentation/components/Details/PorjectUtils/ProjectHeader.vue
@@ -99,7 +99,8 @@ const actionList = (id: number) => [
         </span>
       </div>
     </div>
-<!-- 
+    
+<!--
     <div class="project-actions" :aria-label="t('Project actions')">
       <DropList :actionList="actionList(Number(route.params.id))" />
     </div> -->
