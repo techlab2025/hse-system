5201af0e74715ae0317e14b4d35d40e5fca3314f
Mohab <mohabmohamedd772@gmail.com>
2026-09-21 10:10:47 +0300

    update position

--- src/features/Organization/Project/Presentation/components/UpdatedProjectFlow/Steps/ProjectPositionsStep.vue
+++ src/features/Organization/Project/Presentation/components/UpdatedProjectFlow/Steps/ProjectPositionsStep.vue
@@ -24,7 +24,7 @@ const projectCustomLocationController = ProjectCustomLocationController.getInsta
 const isLoadingLocations = ref(false)
 const locationsError = ref('')
 const hierarchyParams = computed(
-  () => new IndexHerikalyParams('', 1, 30, 0, false, props.projectId ?? null),
+  () => new IndexHerikalyParams('', 1, 30, 0, false, undefined),
 )
 
 const employeeParams = (hierarchyId: number | null = null) =>
